ebe9a54a3912b84b31c114eb78bfca03d7d7c9da
Mauricio Suarez <msuarezm1994@gmail.com>
2022-08-29 19:37:36 -0500

    metodo add no funcional

--- src/02-methods.ts
+++ src/02-methods.ts
@@ -13,7 +13,21 @@ export class MyDate {
     return `${this.day}/${this.month}/${this.year}`;
   }
 
+  add (amount: number, type: 'days'|'months'|'years'){
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type ==='months'){
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+
 }
 
 const myDate = new MyDate(2022, 7, 15);
 console.log(myDate.printFormat());
+myDate.add(3, 'days');
+console.log(myDate.printFormat())
